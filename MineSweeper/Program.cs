0fca46b44935eb6fb51243c4d08ac52c26b5cb9e
Oliver <oliver.nordin@elev.ga.ntig.se>
2021-09-29 14:32:15 +0200

    first comitt

---
+++ MineSweeper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MineSweeper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ MineSweeper/SweeperGrid.cs
@@ -0,0 +1,61 @@
+using System;
+
+namespace MineSweeper
+{
+    public class SweeperGrid
+    {
+        static int gridX = 10;
+        static int gridY = 10;
+
+        public int[,] gridArr = new int[gridX, gridY];
+
+        public SweeperGrid()
+        {
+            Random gen = new Random();
+            for (int i = 0; i < gridX; i++)
+            {
+                for (int j = 0; j < gridY; j++)
+                {
+                    gridArr[i,j] = 9;
+                }
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                int randomI = gen.Next(gridX);
+                int randomJ = gen.Next(gridY);
+
+                gridArr[randomI, randomJ] = 10;            
+            }    
+        } 
+        
+        public bool CheckPosition(int x, int y)
+        {
+            if (x >= 0 && x <= gridX && y >= 0 && y <= gridY)
+            {
+                if (gridArr[x, y] == 10)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void Draw()
+        {
+            for (int i = 0; i < gridX; i++)
+            {
+                for (int j = 0; j < gridY; j++)
+                {
+                    if (gridArr[i, j] == 10)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write("█");
+                    }
+                }
+            }
+        }
+    }
+}
